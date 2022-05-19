1ffecf07a3539e1fcdda05acd8c76ef5abd09599
osigie <90554061+osigie@users.noreply.github.com>
2022-05-19 03:14:35 +0100

    added environment file and added axios

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+
+import Header from "./Header"
+import Watched from "./Watched"
+import WatchList from "./WatchList"
+import Add from "./Add"
+import ResultCard from "./ResultCard"
+
+
+
+export {
+    Header,
+    Watched,
+    WatchList,
+    Add,
+    ResultCard 
+}
