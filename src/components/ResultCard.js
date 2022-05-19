c5f3ba4318279928095b8b5b785ea1e8b2955df6
osigie <90554061+osigie@users.noreply.github.com>
2022-05-19 03:15:06 +0100

    set up result card and add component

---
+++ src/components/ResultCard.js
@@ -0,0 +1,30 @@
+const resultCard = ({ movie }) => {
+  return (
+    <div className="result-card">
+      <div className="poster-wrapper">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={`${movie.title} poster`}
+          />
+        ) : (
+          <div className="filler-poster"> </div>
+        )}
+      </div>
+
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.title}</h3>
+          <h4 className="released-date">
+            {movie.release_date ? movie.release_date.substring(0, 4) : "-"}
+          </h4>
+        </div>
+        <div className="controls">
+          <button className="btn">Add to watchlist</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default resultCard;
